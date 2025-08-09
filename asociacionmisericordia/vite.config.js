9e9c4ab4bdb0eda8764732e42e6bd8168a7f8352
Laura Camila Jiménez Zamora <lauracamilajimenezzamora@MacBook-Air-de-Laura.local>
2025-08-08 23:58:16 -0300

    version4

--- asociacionmisericordia/vite.config.js
+++ asociacionmisericordia/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Esta línea le dice a Vite que la ruta base es la raíz del dominio
+  base: '/'
+});
